42b74215a28d61943d45ee670f5ed8b95c6a04d3
marsalansohail123 <arsalan.sohail1934@gmail.com>
2024-06-26 18:54:56 +0500

    Working

--- Backend/app.js
+++ Backend/app.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 4000;
-const mongoose = require("mongoose")
-const mongoDBURI = "mongodb+srv://arsalansohail1934:Fa5RG1b2dAqsVqSR@cluster0.kbgfzxu.mongodb.net/";
+const mongoose = require("mongoose");
+const TodoModel = require("./models/todoSchema");
+// const TodoModel = require("./models/todoSchema");
+const mongoDBURI = "";
+const cors = require("cors")
 
+app.use(cors())
 app.use(express.json())
 
 mongoose.connect(mongoDBURI)
@@ -13,5 +17,44 @@ mongoose.connect(mongoDBURI)
     }).catch((err) => {
         console.log("Oops!", err)
     })
+
+// Add TODO
+
+app.post("/api/react_todo", (request, response) => {
+    const body = request.body;
+    const ObjToSend = {
+        todo: body.todo,
+    }
+    TodoModel.create(ObjToSend)
+        .then((data) => response.json({
+            message: "Data Send Successfully",
+            status: true,
+            data
+        })).catch((err) => {
+            response.json({
+                message: `Internal Server Error ${err}`,
+                status: false
+            })
+        })
+    console.log(body)
+})
+
+// GET TODO DATA
+
+app.get("/api/react_todo", ((request, response) => {
+    TodoModel.find({})
+        .then((data) => {
+            response.json({
+                message: "Data Get Succesfully",
+                status: true,
+                data, data
+            })
+        }).catch((err) => {
+            response.json({
+                message: `Internal Server Error ${err}`,
+                status: false
+            })
+        })
+}))
 
 app.listen(port, () => console.log(`Server running on PORT ${port}`))
--- Frontend/todo-frontend/src/App.jsx
+++ Frontend/todo-frontend/src/App.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./App.css"
+import axios from 'axios';
 
 function TodoApp() {
+
+  // Add TODO
+  const [inputVal, setInputVal] = useState("");
+  const addTodo = () => {
+    if (!inputVal) {
+      alert("Please add TODO")
+      return
+    }
+    // console.log(inputVal)
+    const objToSend = {
+      todo: inputVal
+    }
+    axios.post('http://localhost:8000/api/react_todo', objToSend)
+      .then((res) => {
+        console.log(res)
+        setInputVal("")
+      }).catch((err) => {
+        console.log(err)
+      })
+  }
+
+  // GET ALL TODOS
+  const [todoArr, setTodoArr] = useState([])
+  useEffect(() => {
+    axios.get("http://localhost:8000/api/react_todo")
+      .then((res) => {
+        setTodoArr(res.data.data)
+      }).catch((err) => console.log(err))
+  }, [])
+  // console.log(todoArr)
+
   return (
     <div className="todo-app">
       <header>
@@ -13,17 +45,21 @@ function TodoApp() {
             type="text"
             placeholder="Add new todo..."
             className="new-todo-input"
+            value={inputVal}
+            onChange={(e) => { setInputVal(e.target.value) }}
           />
-          <button onClick={() => console.log("hello")} className="add-todo-btn">+</button>
+          <button onClick={() => addTodo()} className="add-todo-btn">+</button>
         </div>
         <ul className="todo-list">
-          <li className="todo-item">
-            <label htmlFor="todo-1">TODO LIST 1</label>
-            <div className='func-btn'>
-              <button className="edit-todo-btn">Edit</button>
-              <button className="delete-todo-btn">Delete</button>
-            </div>
-          </li>
+          {todoArr.map((data) => (
+            <li id={data._id} className="todo-item">
+              <label htmlFor="todo-1">{data.todo}</label>
+              <div className='func-btn'>
+                <button className="edit-todo-btn">Edit</button>
+                <button className="delete-todo-btn">Delete</button>
+              </div>
+            </li>
+          ))}
         </ul>
       </main>
     </div>
